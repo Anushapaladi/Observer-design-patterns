4bd5a6bb69dd76f30a54323bb2e86a11bcd745b2
Anushapaladi <89947760+Anushapaladi@users.noreply.github.com>
2021-09-06 20:46:50 -0500

    Delete sale.java

--- sale.java
+++
@@ -1,9 +0,0 @@
-package anusha.observer.pattern;
-
-public interface sale {
-	public void addsalebenefactor(buyer buyer);
-
-	public void removesalebenefactor(buyer buyer);
-
-	public void notifysalebenefactor(String message);
-}
